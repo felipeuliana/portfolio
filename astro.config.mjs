cae556bc6b3e8e1098d1bc5acd84425be784f03a
Felipe Uliana <v.uliana@gmail.com>
2026-04-21 17:51:05 -0300

    feat: update Astro configuration and add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-
+import markdoc from '@astrojs/markdoc';
+import node from '@astrojs/node';
+import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({
+    mode: 'standalone',
+  }),
+  base: '/portfolio',
   integrations: [react(), markdoc(), keystatic()],
   root: '',
+  site: 'https://felipeuliana.github.io',
 });
