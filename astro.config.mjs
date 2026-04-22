1c0001d093e376d5e00c733f85311eb30701d34c
Felipe Uliana <v.uliana@gmail.com>
2026-04-22 20:46:26 -0300

    chore: Add Astro Netlify adapter
    - Install @astrojs/netlify dependency;
    - Add netilify adapter to Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
-  output: 'static',
   root: '',
+  adapter: netlify(),
 });
