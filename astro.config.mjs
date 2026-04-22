08ba001596d7b7e38ff70486c1f2bf45cb7819eb
Felipe Uliana <v.uliana@gmail.com>
2026-04-22 20:18:42 -0300

    chore: Rollback Github Pages deployment infrastructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,12 @@
 
 import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
-  base: '/portfolio',
   integrations: [react(), markdoc(), keystatic()],
   output: 'static',
   root: '',
-  site: 'https://felipeuliana.github.io',
 });
